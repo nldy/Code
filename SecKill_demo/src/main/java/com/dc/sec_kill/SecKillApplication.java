19c00e715f14b6dab0d9fc950055bc67877a2bc0
nldy <1531990336@qq.com>
2019-03-16 20:53:07 +0800

    secKill

---
+++ SecKill_demo/src/main/java/com/dc/sec_kill/SecKillApplication.java
@@ -0,0 +1,13 @@
+package com.dc.sec_kill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecKillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecKillApplication.class, args);
+    }
+
+}
