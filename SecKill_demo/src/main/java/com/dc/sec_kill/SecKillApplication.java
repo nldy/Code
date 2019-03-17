3551951bc8225e4decb2ae6c6ff67a7887da7bad
nldy <1531990336@qq.com>
2019-03-17 23:06:53 +0800

    显示数据库数据

--- SecKill_demo/src/main/java/com/dc/sec_kill/SecKillApplication.java
+++ SecKill_demo/src/main/java/com/dc/sec_kill/SecKillApplication.java
@@ -1,9 +1,11 @@
 package com.dc.sec_kill;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.dc.sec_kill.dao")
 public class SecKillApplication {
 
     public static void main(String[] args) {
